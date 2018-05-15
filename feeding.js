722da49da7252fa3d185503dda68214b8b679d53
Mark Garnick <mgarnick1@outlook.com>
2018-05-14 20:22:40 -0600

    first commit

---
+++ feeding.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+'use strict'
+var weight = prompt("What is your child's weight in kilograms");
+var kcalorie = prompt("Please choose the ounces per day your child requires: 90, 100, 110");
+var result = weight * kcalorie;
+
+alert("Your child should have " + result + " milliliters per day");
