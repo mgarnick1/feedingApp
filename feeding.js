49fccc88fff23722395a1598a0dde3db020ed661
Mark Garnick <mgarnick1@outlook.com>
2018-05-16 19:37:27 -0600

    Updated results to be a function and fixed css reference

--- feeding.js
+++ feeding.js
@@ -2,6 +2,9 @@
 'use strict'
 var weight = prompt("What is your child's weight in kilograms");
 var kcalorie = prompt("Please choose the ounces per day your child requires: 90, 100, 110");
-var result = weight * kcalorie;
 
-alert("Your child should have " + result + " milliliters per day");
+function results(weight, calorie) {
+    return weight * kcalorie;
+}
+
+alert("Your child should have " + results(weight, kcalorie) + " milliliters per day");
